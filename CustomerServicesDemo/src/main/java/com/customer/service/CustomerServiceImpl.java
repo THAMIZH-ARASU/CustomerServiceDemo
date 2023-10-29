876ada38b81dcf0bf5e0ee2579e110a66dd620c3
THAMIZHARASU <THAMIZHARASU@LAPTOP-8GEBQS9O>
2023-10-29 15:46:32 +0530

    Initial Commit

---
+++ CustomerServicesDemo/src/main/java/com/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,47 @@
+package com.customer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	@Autowired
+    private final CustomerRepository customerRepository;
+
+    
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer customer) {
+        if (customerRepository.existsById(id)) {
+            customer.setId(id);
+            return customerRepository.save(customer);
+        }
+        return null; // Handle error, customer not found
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
---
+++ CustomerServicesDemo/src/main/java/com/customer/service/NewCustomerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCustomerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCustomerServicesApplication.class, args);
+	}
+
+}
